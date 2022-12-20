73f8a33fd977357fd5519d11f2286cba3d0c3416
Dmitry Korochkin <abramslost@gmail.com>
2022-12-20 20:46:52 +0300

    formating files

--- src/modules/modals.ts
+++ src/modules/modals.ts
@@ -1,105 +1,103 @@
 export const modals = (): void => {
-    const bindModal = (triggerSelector: string, modalSelector: string, closeSelector: string, closeClickOverlay = true): void => {
-      const triggers: NodeListOf<HTMLElement> = document.querySelectorAll(triggerSelector);
-      const modal: HTMLElement = document.querySelector(modalSelector) as HTMLDivElement;
-      const close: HTMLElement  = document.querySelector(closeSelector) as HTMLDivElement;
-      const windows: NodeListOf<Element> = document.querySelectorAll('[data-modal]');
-      const scroll: number = calcScroll();
-  
-      triggers.forEach((trigger: Element): void => {
-        trigger.addEventListener("click", (e: Event): void => {
-          if (e.target) {
-            e.preventDefault();
-          }
-  
-          windows.forEach(window => {
-            (window as HTMLElement).style.display = "none"
-          });
-  
-          (modal as HTMLElement).style.display = "block";
-          document.body.style.overflow = "hidden";
-          document.body.style.marginRight = `${scroll}px`
-        });
-      });
-  
-      const windowModal = (): void => {
+  const bindModal = (triggerSelector: string, modalSelector: string, closeSelector: string, closeClickOverlay = true): void => {
+    const triggers: NodeListOf<HTMLElement> = document.querySelectorAll(triggerSelector);
+    const modal: HTMLElement = document.querySelector(modalSelector) as HTMLDivElement;
+    const close: HTMLElement = document.querySelector(closeSelector) as HTMLDivElement;
+    const windows: NodeListOf<Element> = document.querySelectorAll('[data-modal]');
+    const scroll: number = calcScroll();
+
+    triggers.forEach((trigger: Element): void => {
+      trigger.addEventListener("click", (e: Event): void => {
+        if (e.target) {
+          e.preventDefault();
+        }
+
         windows.forEach(window => {
           (window as HTMLElement).style.display = "none"
-        })
-      }
-  
-      const closeModal = (): void => {
-        (modal as HTMLElement).style.display = "none";
-        document.body.style.overflow = "";
-      };
-  
-      const hiddenScroll = (): void => {
-        document.body.style.marginRight = `0px`
-      }
-  
-      document.addEventListener("keydown", function (e) {
-        const key = e.key;
-        if (key === "Escape") {
-          closeModal();
-        }
+        });
+
+        (modal as HTMLElement).style.display = "block";
+        document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`
       });
-  
-      close.addEventListener("click", () => {
+    });
+
+    const windowModal = (): void => {
+      windows.forEach(window => {
+        (window as HTMLElement).style.display = "none"
+      })
+    }
+
+    const closeModal = (): void => {
+      (modal as HTMLElement).style.display = "none";
+      document.body.style.overflow = "";
+    };
+
+    const hiddenScroll = (): void => {
+      document.body.style.marginRight = `0px`
+    }
+
+    document.addEventListener("keydown", function (e) {
+      const key = e.key;
+      if (key === "Escape") {
+        closeModal();
+      }
+    });
+
+    close.addEventListener("click", () => {
+      windowModal();
+      closeModal();
+      hiddenScroll();
+
+    });
+    modal.addEventListener("click", (e) => {
+
+      if (e.target === modal && closeClickOverlay) {
         windowModal();
         closeModal();
         hiddenScroll();
-  
-      });
-      modal.addEventListener("click", (e) => {
-  
-        if (e.target === modal && closeClickOverlay) {
-          windowModal();
-          closeModal();
-          hiddenScroll();
-        }
-      });
-    };
-  
-    const showModalByTime = (selector: string, time: number): void => {
-      setTimeout(function () {
-
-        let displays : string;
-        document.querySelectorAll('[data-modal]').forEach(item => {
-          if(getComputedStyle(item).display !== 'none') {
-            displays = 'block'
-          };
-          if(!displays) {
-            const selectorShowModal: HTMLElement = document.querySelector(selector) as HTMLElement;
-            selectorShowModal.style.display = "block";
-            document.body.style.overflow = "hidden";
-          }
-        })
-
-
-      }, time);
-    };
-  
-    const calcScroll = (): number => {
-  
-      const div: HTMLElement = document.createElement('div');
-  
-      div.style.width = '50px';
-      div.style.height = '50px';
-      div.style.overflowY = 'scroll';
-      div.style.visibility = 'hidden';
-  
-      document.body.appendChild(div);
-      const scrollWidth: number = div.offsetWidth - div.clientWidth;
-      div.remove()
-  
-      return scrollWidth
-  
-    }
-  
-    bindModal('.button-design','.popup-design', '.popup-design .popup-close');
-    bindModal('.button-consultation','.popup-consultation', '.popup-consultation .popup-close');
-    
-    showModalByTime(".popup-consultation", 3000);
+      }
+    });
   };
-  
-  
+
+  const showModalByTime = (selector: string, time: number): void => {
+    setTimeout(function () {
+
+      let displays: string;
+      document.querySelectorAll('[data-modal]').forEach(item => {
+        if (getComputedStyle(item).display !== 'none') {
+          displays = 'block'
+        };
+        if (!displays) {
+          const selectorShowModal: HTMLElement = document.querySelector(selector) as HTMLElement;
+          selectorShowModal.style.display = "block";
+          document.body.style.overflow = "hidden";
+        }
+      })
+    }, time);
+  };
+
+  const calcScroll = (): number => {
+
+    const div: HTMLElement = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    const scrollWidth: number = div.offsetWidth - div.clientWidth;
+    div.remove()
+
+    return scrollWidth
+
+  }
+
+  bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+  bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+
+  showModalByTime(".popup-consultation", 3000);
+};
+
+
