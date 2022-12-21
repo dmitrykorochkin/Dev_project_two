11e71c861cce15de2bc15af64e28ac6cf14aa88f
Dmitry Korochkin <abramslost@gmail.com>
2022-12-21 14:52:07 +0300

    add path sliders.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import  {modals}  from './modules/modals'; 
+import  {modals}  from './modules/modals';
+import {sliders} from './modules/sliders'
 
 window.addEventListener('DOMContentLoaded', (): void => {
  
-
-
   modals();
+  sliders();
 });
 
