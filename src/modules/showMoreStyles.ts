ddd065b320f95ac6a55cd5d5f9ea998d18fa2ce5
Dmitry Korochkin <abramslost@gmail.com>
2023-01-03 11:06:09 +0300

    fix

--- src/modules/showMoreStyles.ts
+++ src/modules/showMoreStyles.ts
@@ -1,38 +1,18 @@
-import { getResource } from "../services/request";
 
-export const showMoreStyles = (trigger: string, wrapper: string): void => {
-    
+export const showMoreStyles = (trigger: string, styles: string): void => {
+    const cards: NodeListOf<HTMLDivElement> = document.querySelectorAll(styles);
     const button: Element = document.querySelector(trigger) as HTMLButtonElement;
 
-    button.addEventListener('click', function() {
-        getResource('http://localhost:3000/styles')
-            .then(res => createCards(res))
+    cards.forEach(card => {
+        card.classList.add('animated', 'fadeInUp')
+    })
 
-        this.remove();    
-    });
-
-    const createCards = (response: any): void => {
-        response.forEach(item => {
-            let card: HTMLDivElement = document.createElement('div')
-            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1')
-
-            card.innerHTML = `
-                <div class=styles-block>
-                    <img src=${item.src} alt>
-                    <h4>${item.title}</h4>
-                    <a href="${item.link}">Подробнее</a>
-                </div>
-            `
-            document.querySelector(wrapper).append(card);
+    button.addEventListener('click', (): void => {
+        cards.forEach(card => {
+            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1',)
+            card.classList.remove('hidden-lg', 'hidden-sm', 'hidden-xs', 'hidden-md',)
         })
-        
-    }
-};
 
-// <div class="hidden-lg hidden-md hidden-sm hidden-xs styles-2">
-// <div class=styles-block>
-//     <img src=src/img/styles-5.jpg alt>
-//     <h4>Пастелью</h4>
-//     <a href="#">Подробнее</a>
-// </div>
-// </div>
+        button.remove()
+    })
+};
