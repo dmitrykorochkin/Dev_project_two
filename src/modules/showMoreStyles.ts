50b2ee2adb3cf99a899c8ad77928c58bc7988fe4
Dmitry Korochkin <abramslost@gmail.com>
2023-01-03 11:07:43 +0300

    update shomMoreStyles

--- src/modules/showMoreStyles.ts
+++ src/modules/showMoreStyles.ts
@@ -1,17 +1,30 @@
-export const showMoreStyles = (trigger: string, styles: string): void => {
-    const cards: NodeListOf<HTMLDivElement> = document.querySelectorAll(styles);
+import { getResource } from "../services/request";
+
+export const showMoreStyles = (trigger: string, wrapper: string): void => {
+    
     const button: Element = document.querySelector(trigger) as HTMLButtonElement;
 
-    cards.forEach(card => {
-        card.classList.add('animated', 'fadeInUp')
-    })
+    button.addEventListener('click', function() {
+        getResource('/src/db.json')
+            .then(res => createCards(res.styles))
 
-    button.addEventListener('click', (): void => {
-        cards.forEach(card => {
-            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1',)
-            card.classList.remove('hidden-lg', 'hidden-sm', 'hidden-xs', 'hidden-md',)
+        this.remove();    
+    });
+
+    const createCards = (response: any): void => {
+        response.forEach(({src, title, link}):void => {
+            let card: HTMLDivElement = document.createElement('div')
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1')
+
+            card.innerHTML = `
+                <div class=styles-block>
+                    <img src=${src} alt>
+                    <h4>${title}</h4>
+                    <a href="${link}">Подробнее</a>
+                </div>
+            `;
+            document.querySelector(wrapper).append(card);
         })
-
-        button.remove()
-    })
+        
+    }
 };
