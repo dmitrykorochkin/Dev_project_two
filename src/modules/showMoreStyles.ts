dcbb4a236cb074bed6c8fd3b0193bacad3a1b01b
Dmitry Korochkin <abramslost@gmail.com>
2023-01-08 17:03:25 +0300

    fix build prev branch

--- src/modules/showMoreStyles.ts
+++ src/modules/showMoreStyles.ts
@@ -1,8 +1,8 @@
 import { getResource } from "../services/request";
 export interface IStyles {src: string, title: string, link: string}
 
-export const showMoreStyles = (trigger: string, styles: string): void => {
-    const cards: NodeListOf<HTMLDivElement> = document.querySelectorAll(styles);
+export const showMoreStyles = (trigger: string, wrapper: string): void => {
+
     const button: Element = document.querySelector(trigger) as HTMLButtonElement;
 
     
