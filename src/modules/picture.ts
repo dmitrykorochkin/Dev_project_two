af38c683ef4a86a6a9359ed69f206629f40e7526
Dmitry Korochkin <abramslost@gmail.com>
2023-01-08 18:16:50 +0300

    fix block

--- src/modules/picture.ts
+++ src/modules/picture.ts
@@ -2,30 +2,27 @@ export const picture = (imgSelector: string): void => {
     const blocks: NodeListOf<HTMLElement> = document.querySelectorAll(imgSelector);
 
     const showImg = (block: any): void => {
-        const img: HTMLImageElement = (block).querySelector('img');
+        const img: HTMLImageElement = block.querySelector('img');
         img.src = img.src.slice(0, -4) + `-1.png`;
 
-        block.querySelectorAll('p: not(.sizes-hit)').forEach((p: HTMLElement) => {
+        block.querySelectorAll('p:not(.sizes-hit)').forEach((p: HTMLElement) => {
             p.style.display = 'none';
         })
     }
     const hideImg = (block: any): void => {
-        const img: HTMLImageElement = (block).querySelector('img');
-        img.src = img.src.slice(0, -6) + `1.png`;
+        const img: HTMLImageElement = block.querySelector('img');
+        img.src = img.src.slice(0, -6) + `.png`;
 
-        block.querySelectorAll('p: not(.sizes-hit)').forEach((p: HTMLElement) => {
+        block.querySelectorAll('p:not(.sizes-hit)').forEach((p: HTMLElement) => {
             p.style.display = 'block';
         })
-
-        blocks.forEach(block => {
-            block.addEventListener('mouseover', () => {
-                showImg(block)
-            });
-        })
-        blocks.forEach(block => {
-            block.addEventListener('mouseout', () => {
-                hideImg(block)
-            });
-        });
     };
+    blocks.forEach(block => {
+        block.addEventListener('mouseover', (): void => {
+            showImg(block)
+        });
+        block.addEventListener('mouseout', (): void => {
+            hideImg(block)
+        });
+    })
 }
