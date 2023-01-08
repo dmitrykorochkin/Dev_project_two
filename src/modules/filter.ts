a92026d8cd4162608799c9a60167115162e6bad7
Dmitry Korochkin <abramslost@gmail.com>
2023-01-08 14:07:24 +0300

    add filter.ts

--- src/modules/filter.ts
+++ src/modules/filter.ts
@@ -13,21 +13,21 @@ export const filter = (): void => {
     const markLovers: NodeListOf<HTMLLIElement> = wrapper.querySelectorAll('.portfolio-menu');
     const markChef: NodeListOf<HTMLLIElement> = wrapper.querySelectorAll('.portfolio-menu');
     const markGuy: NodeListOf<HTMLLIElement> = wrapper.querySelectorAll('.portfolio-menu');
+    const markGirl: NodeListOf<HTMLLIElement> = wrapper.querySelectorAll('.portfolio-menu');
     const no: HTMLElement = document.querySelector('.portfolio-menu')  as HTMLElement;
 
-    const typeFilter = (markType: string): void => {
-        markAll.forEach(mark => {
+    const typeFilter = (markType: any): void => {
+        markAll.forEach((mark) => {
             mark.style.display = 'none';
             mark.classList.remove('animated', 'fadeIn');
         })
-    }
 
-    no.style.display = 'none';
-    no.classList.remove('animated', 'fadeOut');
+        no.style.display = 'none';
+        no.classList.remove('animated', 'fadeOut');
 
     if(markType) {
-        markType.forEach(mark => {
-            mark.style.display = 'block';
+        markType.forEach((mark: HTMLElement) => {
+            (mark as HTMLElement).style.display = 'block';
             mark.classList.add('animated', 'fadeIn');
         });
     } else {
@@ -40,6 +40,32 @@ export const filter = (): void => {
     });
     btnLovers.addEventListener('click', (): void => {
         typeFilter(markLovers);
+    });
+    btnChef.addEventListener('click', (): void => {
+        typeFilter(markChef);
+    });
+    btnGuy.addEventListener('click', (): void => {
+        typeFilter(markGuy);
+    });
+    btnGirl.addEventListener('click', (): void => {
+        typeFilter(markGirl);
+    });
+    btnGrandmother.addEventListener('click', (): void => {
+        typeFilter();
+    });
+    btnGranddad.addEventListener('click', (): void => {
+        typeFilter();
     })
+
+    menu.addEventListener('click', (e: Event): void => {
+        const target: any = e.target;
+        if(target && target.tagName == 'LI') {
+            items.forEach(btn => btn.classList.remove('active'))
+            target.classList.add('active');
+        }
+    })
+    }
+
+    
     
 }
