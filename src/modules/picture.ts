26899746f041f3972c5c76dfb3283a2fce8c983c
Dmitry Korochkin <abramslost@gmail.com>
2023-01-08 17:57:30 +0300

    add picture.ts

--- src/modules/picture.ts
+++ src/modules/picture.ts
@@ -1,3 +1,33 @@
 export const picture = (imgSelector: string): void => {
     const blocks: NodeListOf<HTMLElement> = document.querySelectorAll(imgSelector);
+
+    const showImg = (block: any): void => {
+        const img: HTMLImageElement = (block).querySelector('img');
+        img.src = img.src.slice(0, -4) + `-1.png`;
+
+        block.querySelectorAll('p: not(.sizes-hit)').forEach((p: HTMLElement) => {
+            p.style.display = 'none';
+        })
+    }
+    const hideImg = (block: any): void => {
+        const img: HTMLImageElement = (block).querySelector('img');
+        img.src = img.src.slice(0, -6) + `1.png`;
+
+        block.querySelectorAll('p: not(.sizes-hit)').forEach((p: HTMLElement) => {
+            p.style.display = 'block';
+        })
+
+        blocks.forEach(block => {
+            block.addEventListener('mouseover', () => {
+                showImg(block)
+            });
+        })
+        blocks.forEach(block => {
+            block.addEventListener('mouseout', () => {
+                hideImg(block)
+            });
+        });
+    };
+
+
 }
