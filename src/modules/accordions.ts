73677eac25077b32ab536445ecc4df28a1cf21ea
Dmitry Korochkin <abramslost@gmail.com>
2023-01-09 20:01:40 +0300

    add accordions.ts

---
+++ src/modules/accordions.ts
@@ -0,0 +1,9 @@
+export const accordion = (trigerSelector: string) => {
+    const btns: NodeListOf<HTMLElement> = document.querySelectorAll(trigerSelector); 
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', function() {
+            this.
+        })
+    })
+}
