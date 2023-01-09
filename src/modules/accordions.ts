38d96bbe41ea69ceda06e4bc91ca146e23366663
Dmitry Korochkin <abramslost@gmail.com>
2023-01-09 20:08:22 +0300

    add accordions

--- src/modules/accordions.ts
+++ src/modules/accordions.ts
@@ -1,9 +1,16 @@
-export const accordion = (trigerSelector: string) => {
-    const btns: NodeListOf<HTMLElement> = document.querySelectorAll(trigerSelector); 
+export const accordion = (triggerSelector: string) => {
+    const btns: NodeListOf<HTMLElement> = document.querySelectorAll(triggerSelector);
 
     btns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            this.
-        })
-    })
-}
+        btn.addEventListener('click', function () {
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classlist.toggle('active-content');
+
+            if (this.classList.contains('active-style')) {
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + 'px';
+            } else {
+                this.nextElementSibling.style.maxHeight = '0px'
+            }
+        });
+    });
+};
