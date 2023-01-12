df9d3e2583026fbdc1705d03f23d40860e53f7de
Dmitry Korochkin <abramslost@gmail.com>
2023-01-12 20:00:22 +0300

    fix

--- src/modules/drop.ts
+++ src/modules/drop.ts
@@ -1,6 +1,6 @@
 export const drop = (): void => {
 
-    const fileInputs: NodeListOf<Element> = document.querySelectorAll('[name="upload"]');
+    const fileInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll('[name="upload"]');
 
     ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
         fileInputs.forEach(input => {
@@ -13,8 +13,9 @@ export const drop = (): void => {
     }
 
     const highlight = (item: any): void => {
-        item.closest('.file_upload').style.border = '2px solid pink';
-        item.closest('.file_upload').style.backgroundColor = 'rgba(0,0,0, .1)';
+        const closest: HTMLElement = item.closest('.file_upload')
+        closest.style.border = '2px solid pink';
+        closest.style.backgroundColor = 'rgba(0,0,0, .1)';
     }
     const unhighlight = (item: any): void => {
         item.closest('.file_upload').style.border = 'none';
@@ -26,18 +27,18 @@ export const drop = (): void => {
     }
     ['dragenter', 'dragover'].forEach(eventName => {
         fileInputs.forEach(input => {
-            input.addEventListener(eventName, ()=> highlight(input), false);
+            input.addEventListener(eventName, ()=> highlight(input as HTMLElement), false);
         });
     });
     ['dragleave', 'drop'].forEach(eventName => {
         fileInputs.forEach(input => {
-            input.addEventListener(eventName, ()=> unhighlight(input), false);
+            input.addEventListener(eventName, ()=> unhighlight(input as HTMLElement), false);
         });
     });
 
     fileInputs.forEach(input => {
-        input.addEventListener('drop', (e: Event): void => {
-            input.files = e.dataTransfer.files;
+        input.addEventListener('drop', (e: DragEvent): void => {
+            input.files = (e.dataTransfer as DataTransfer).files;
             const file = (input.files as FileList)[0];
             const fileName:string = file.name.split('.')[0];
             const dots: string = fileName.length > 6 ? '...' : '.';
